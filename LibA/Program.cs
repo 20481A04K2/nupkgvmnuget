3f2f0c42eec27f41f8789971f676a3724dbad80c
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-05-23 12:04:00 +0530

    Update and rename UtilityA.cs to Program.cs

---
+++ LibA/Program.cs
@@ -0,0 +1,6 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "✅ Hello from LibA (Port 8081)");
+
+app.Run("http://0.0.0.0:8081");
--- LibA/UtilityA.cs
+++
@@ -1,6 +0,0 @@
-namespace LibA;
-
-public class UtilityA
-{
-    public string GetMessage() => "Hello from LibA v1.0.0";
-}
